76b870f5dc3f9ad8f875e5b313ca07101edca439
ZacharyChen <zacharyzekaichen@gmail.com>
2020-11-30 15:47:36 +0800

    Git Test.

---
+++ GitTest/src/com/chenzk/gittest/GitTest.java
@@ -0,0 +1,11 @@
+package com.chenzk.gittest;
+
+/**
+ * @author chenzk
+ * @create 2020-11-30 15:42
+ */
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("Git Test!");
+    }
+}
