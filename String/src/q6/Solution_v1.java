6285f1dcc6b516e64401ba3dd08a5969f95d05fc
ZacharyChen <zacharyzekaichen@gmail.com>
2020-11-30 22:43:42 +0800

    q6 solution v1 20201130

---
+++ String/src/q6/Solution_v1.java
@@ -0,0 +1,47 @@
+package q6;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * @author chenzk
+ * @create 2020-11-30 19:10
+ */
+public class Solution_v1 {
+    public static void main(String[] args) {
+        System.out.println(convert("LEETCODEISHIRING",1));
+    }
+
+    public static String convert(String s, int numRows){
+        List<StringBuilder> strArr = new ArrayList<>();
+        for(int i = 0;i < Math.min(numRows, s.length());i++){
+            strArr.add(new StringBuilder());
+        }
+
+        int orderRow = 0;
+        int orderChar = 0;
+        boolean downFlag = true;
+
+        while(true){
+            //添加这一步是正确的。
+            strArr.get(orderRow).append(s.charAt(orderChar));
+            //分情况讨论
+            if(numRows != 1){
+                if(downFlag) {orderRow++;}
+                else if(!downFlag) {orderRow--;}
+
+                if(orderRow == numRows - 1) { downFlag = false;}
+                else if(orderRow == 0) { downFlag = true;}
+            }
+            orderChar++;
+            if(orderChar == s.length()) {break;}
+        }
+
+        StringBuilder returnValue = new StringBuilder();
+        for(StringBuilder str : strArr){
+            returnValue.append(str);
+        }
+
+        return new String(returnValue);
+    }
+}
